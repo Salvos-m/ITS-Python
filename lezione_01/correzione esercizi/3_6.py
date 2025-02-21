9e53bcce19e36619ec5eb71fe47adf3a3c2bcfc6
Salvos-m <salvatore02899@gmail.com>
2025-02-21 17:13:11 +0100

    Aggiornamenti

--- lezione_01/correzione esercizi/3_6.py
+++ lezione_01/correzione esercizi/3_6.py
@@ -7,3 +7,27 @@ to the end of your program, informing people that you found a bigger table.
 • Use insert() to add one new guest to the middle of your list.
 • Use append() to add one new guest to the end of your list.
 • Print a new set of invitation messages, one for each person in your list.'''
+
+''''
+guest=["piero angela", "Giorgia", "annalisa", "pippo"] 
+
+escluso = guest.pop()
+print(f"{escluso} sei fuori")
+
+escluso = guest.pop()
+print(f"{escluso} sei fuori")
+
+escluso = guest.pop()
+print(f"{escluso} sei fuori")
+'''
+'''
+l=[1,2,3,5,"s"] 
+#for i in range(len(l))
+
+for i in range(len(l)-1,-1,-1)
+print ("sto rimuovendo lelemento allindice", i)
+l.remove(l[i])
+'''
+
+
+
