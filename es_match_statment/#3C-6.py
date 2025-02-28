10005f482838042138a1b3246d8abd1aa1ff33d9
Salvos-m <salvatore02899@gmail.com>
2025-02-28 10:06:37 +0100

    new

--- es_match_statment/#3C-6.py
+++ es_match_statment/#3C-6.py
@@ -51,8 +51,14 @@ rettili= ["serpente", "lucertola", "tartaruga", "coccodrillo"]
 uccelli= ["aquila", "pappagallo", "gufo", "falco"]
 pesci= ["squalo", "trota", "salmone", "carpa"]
 tutti=[mammiferi,rettili,uccelli,pesci]
+animal_type=
+
+acqua=[pesci,mammiferi[-1]]
+terra=[mammiferi,rettili,uccelli]
+aria=[uccelli]
 
 animale=input("inserisci un animale:")
+habitat=input("inserisci il suo habitat:")
 
 match animale:
     case animale if animale in mammiferi:
@@ -66,5 +72,34 @@ match animale:
     case animale if animale not in tutti:
         print(f"{animale} non appartiene a nessuna delle categorie")
 '''
+
+animale:str=(input("inserisci l'animale "))
+mammiferi:list=["cane","gatto","cavallo","elefante","leone","balena","delfino"]
+rettili:list=["serpente","lucertola","tartaruga","coccodrillo"]
+uccelli:list=["aquila","pappagallo","gufo","falco","cigno","anatra","tacchino"]
+pesci:list=["squalo","trota","salmone","carpa"]
+animali:list=[mammiferi,rettili,pesci,uccelli]
+habitat:list=["aria","acqua","terra"]
+
+match animale:
+  case animale if animale in mammiferi and animale in (mammiferi[0:4]):
+   print(f"{animale} vive nella {habitat[2]}")
+  case animale if animale in mammiferi and animale in (mammiferi[5:7]):
+   print(f"{animale} vive nel {habitat[1]}")
+  case animale if animale in rettili and animale in (rettili[0:2]):
+   print(f"{animale} vive sulla{habitat[2]} ferma")
+  case animale if animale in rettili and animale in (rettili[2:4]):
+   print(f"{animale} vive sull {habitat[1]} e sulla {habitat[2]} ferma")
+  case animale if animale in uccelli and animale in (uccelli[0:3]):
+   print(f"{animale} vive nel {habitat[0]}")
+  case animale if animale in uccelli and animale in (uccelli[4:6]):
+   print(f"{animale} vive nel {habitat[1]}")
+  case animale if animale in uccelli and animale in uccelli[7]:
+   print(f"{animale} vive nel {habitat[2]}")
+  case animale if animale in pesci:
+   print(f"{animale} vive in {habitat[1]}")
+  case _:
+   print(f"sconosciuto")
+
 
 
---
+++ es_pynative/loop/ex_1.py
@@ -0,0 +1,30 @@
+#Print first 10 natural numbers using while loop
+'''
+i=0
+while i<10:
+    i+=1
+    print(i)
+'''
+#------
+'''
+Write a Python code to print the following number pattern using a loop.
+
+1 
+1 2 
+1 2 3 
+1 2 3 4 
+1 2 3 4 5
+'''
+'''
+salary = 8000
+
+def printSalary():
+  salary = 12000
+  print("Salary:", salary)
+  
+printSalary()
+print("Salary:", salary)
+'''
+
+var= "James Bond"
+print(var[2::-1])
