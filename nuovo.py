a8c5d1c468a25573eea08baa76cf14fd35b79875
Salvos-m <salvatore02899@gmail.com>
2025-03-04 00:04:09 +0100

    new

---
+++ nuovo.py
@@ -0,0 +1,12 @@
+addizioni= 7+6+7+6 \
+            +6 
+alta=["soria",
+      2,
+      ]
+dizionario={"ladro":"marco",
+            3:5,
+            }
+
+hello=["he","llo"]
+del hello
+print(hello)
