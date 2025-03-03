2f726d6466b1129af9192c04ddfdd3162bac315c
Salvos-m <salvatore02899@gmail.com>
2025-03-03 19:17:18 +0100

    nuovo

--- es_pynative/loop/esercizio_3.py
+++ es_pynative/loop/esercizio_3.py
@@ -3,3 +3,8 @@ Write a Python code to accept a string from the
 user and display characters present at an even index number.
 '''
 frase="prisencolinesanciusol"
+x=len(frase)
+
+for x in frase:
+    if len(frase)%2==0:
+        print(frase[x])
