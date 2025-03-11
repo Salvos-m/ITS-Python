42cf76cc4f6788d9962a7ca489156ff5c7897029
Salvos-m <salvatore02899@gmail.com>
2025-03-11 11:06:23 +0100

    nuovi_es

--- esercizi its/ITS-Python/nuovo.py
+++ esercizi its/ITS-Python/nuovo.py
@@ -15,3 +15,15 @@ print(keyword.kwlist)
 
 import math
 print(math)
+
+#dizionario  ex10
+'''
+diz_freq={}
+
+while True:
+    numero_inserito=int(input("inserisci un numero: "))
+    if numero_inserito in diz_freq:
+        diz_freq[numero_inserito+=1]
+    else:
+        diz_freq[numero_inserito]=1
+'''
